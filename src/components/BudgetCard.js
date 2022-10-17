f3f88c61e082f81d4cf0d362b247a22277bd791f
david-aihe <doa9200@gmail.com>
2022-10-17 15:04:55 -0400

    feature-BudgetCard-ProgressBar-finished

--- src/components/BudgetCard.js
+++ src/components/BudgetCard.js
@@ -1,15 +1,24 @@
+import { Progress } from "@material-tailwind/react";
+
 const BudgetCard = () => {
+    let title = "Entertainment"
+    let amount = 439
+    let max = 1300
+    let progressPercent = Math.round((amount/max)*100);
+
   return (
     <div className="w-80 m-4 p-4 bg-blue-mid rounded-lg">
         <div className="flex justify-between">
-            <div className="">Title</div>
+            <div className="">{title}</div>
             <div className="">
-                $250 / <span className="">$1000</span>
+                ${amount} / <span className="text-txt-grey">${max}</span>
             </div>
         </div>
-        <div className="progress">
         
-        </div> 
+        <div className="h-4 ">
+            <Progress value={progressPercent} label={`${progressPercent < 10 ? '' : ' '}`} color={`${progressPercent > 70 ? 'red' : 'blue'}`} className="h-6 my-2 bg-txt-grey" />
+        </div>
+         
         <div className="tasks">
 
         </div>
